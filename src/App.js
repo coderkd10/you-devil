ca1ee1f6e10afc5a5eb12cfc9dbd3b63ee302b61
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-10 21:40:00 +0530

    Load heading from start to avoid repositioning on start

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React, { Component, Fragment } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import ContainerDimensions from 'react-container-dimensions';
 import Webcam from './container/Webcam';
-// import VideoPlayer from './presentational/VideoPlayer';
-import VideoPlayer from './container/VideoContainer';
+import VideoContainer from './container/VideoContainer';
 import Heading from './presentational/Heading';
 import styles from './App.module.css';
 
@@ -35,35 +34,39 @@ class App extends Component {
         }) : ({})
       }>
         <ContainerDimensions>
-        {({ width, height }) =>
-          <Fragment>
-            {this.state.videoPlaying ? 
-              <Heading
-                containerWidth={width}
-                onResize={this.onHeadingResize}
-                style={{
-                  height: this.state.headingHeight
-                }}
-              /> : null}
+        {({ width, height }) => {
+          const videoContainerHeight = height - (this.state.videoPlaying ? 
+            this.state.headingHeight : 0);
+          return (<Fragment>
+            <Heading
+              containerWidth={width}
+              onResize={this.onHeadingResize}
+              style={{
+                height: this.state.headingHeight,
+                visibility: this.state.videoPlaying ? 'visible' : 'hidden'
+              }}
+            />
             <div style={{
+              position: 'absolute',
               width: '100%',
-              height: height - this.state.headingHeight
-            }}>
+              height: videoContainerHeight,
+              top: this.state.videoPlaying ? this.state.headingHeight : 0,
+            }}> 
               <Webcam>
                 {({ loading, error, stream }) =>
-                  <VideoPlayer 
+                  <VideoContainer 
                     loading={loading}
                     error={error}
                     stream={stream}
                     onVideoPlayingStart={this.onVideoPlayingStart}
                     width={width}
-                    height={height - this.state.headingHeight}
+                    height={videoContainerHeight}
                   />
                 }
               </Webcam>
             </div>
-          </Fragment>
-        }
+          </Fragment>);
+        }}
         </ContainerDimensions>
       </div>
     )
