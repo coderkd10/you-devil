fd9ca7b3118ae4192030f954697786a799159d06
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-10 19:52:30 +0530

    Separate out pure presentational logic from video player.
    Make a separate container component to hold state.

---
+++ src/container/VideoContainer.js
@@ -0,0 +1,116 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ScaleLoader from 'react-spinners/dist/spinners/ScaleLoader';
+import OrigErrorIcon from 'react-icons/lib/md/error';
+import VideoPlayer from '../presentational/VideoPlayer';
+import makeSizable from '../shared/SizedIcon';
+import AutoScale from '../shared/AutoScale';
+import styles from './VideoContainer.module.css';
+
+const ErrorIcon = makeSizable(OrigErrorIcon);
+
+class VideoContainer extends Component {
+    state = {
+        videoLoading: true,
+        videoHeight: null,
+    };
+
+    componentDidUpdate(prevProps, prevState) {        
+        // check if stream has updated
+        // if yes then play the video
+        if (this.props.stream !== prevProps.stream) {
+            const { error, loading, stream } = this.props;
+            if (!error && !loading) {
+                this.video.srcObject = stream;
+                this.setState({ videoLoading: true });
+                this.video.play().then(() => {
+                    this.setState({ videoLoading: false });
+                    this.props.onVideoPlayingStart();
+                });
+            }
+        }
+    }
+
+    onVideoResize = ({ height, width }) => {
+        this.setState({
+            videoHeight: height,
+        });
+    }
+
+    getVideoTop() {
+        const { videoHeight } = this.state;
+        if (!videoHeight)
+            return 0;
+        const containerHeight = this.props.height;
+        const diff = containerHeight - videoHeight;
+        return 0.35*diff;
+    }
+
+    render() {
+        const { error, loading, height, width } = this.props;
+        const showingLoading = !error && (loading || this.state.videoLoading);
+
+        return <div className={styles.container} 
+            style={{
+                height,
+                width,
+            }}>
+            {showingLoading ?
+                (<div className={styles.center}>
+                    <ScaleLoader 
+                        height={30}
+                        margin='2px'
+                        radius={2}
+                        width={4}
+                    />
+                </div>) : null
+            }
+            {error ?
+                (<div className={styles.center}>
+                    <ErrorIcon width={160} style={{ color: '#b60000' }}/>
+                    <div className={styles.errorText}>Unable to load video</div>
+                </div>) : null
+            }
+            {(!error && !loading) ?
+                (<AutoScale
+                    style={{
+                        position: 'absolute',
+                        left: '50%',
+                        transform: 'translateX(-50%)',
+                        marginTop: this.getVideoTop(),
+                    }}
+                    containerWidth={width}
+                    containerHeight={height}
+                    render={() => 
+                        <VideoPlayer
+                            style={{
+                                visibility: this.state.videoLoading ? 'hidden' : 'visible'                        
+                            }}
+                            videoRef={ref => this.video = ref}
+                            videoMaxWidth={width}
+                            videoMaxHeight={height}
+                        />
+                    }
+                    onResize={this.onVideoResize}
+                />) : null
+            }
+        </div>
+    }
+}
+
+VideoContainer.propTypes = {
+    error: PropTypes.bool.isRequired,
+    loading: PropTypes.bool.isRequired,
+    stream: PropTypes.object,
+    onVideoPlayingStart: PropTypes.func,
+
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+};
+
+VideoContainer.defaultProps = {
+    stream: null,
+    onVideoPlayingStart: () => {},
+}
+
+export default VideoContainer;
--- src/presentational/VideoPlayer.js
+++ src/presentational/VideoPlayer.js
@@ -1,65 +1,33 @@
-import React, { Component } from 'react';
-import classNames from 'classnames';
-import ScaleLoader from 'react-spinners/dist/spinners/ScaleLoader';
-import OrigErrorIcon from 'react-icons/lib/md/error';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './VideoPlayer.module.css';
-import makeSizable from '../shared/SizedIcon';
 
-const ErrorIcon = makeSizable(OrigErrorIcon);
+const VideoPlayer = ({ videoRef, style, videoMaxWidth, videoMaxHeight }) => (
+    <div className={styles.container} style={style}>
+        <video 
+            className={styles.video}
+            muted
+            ref={videoRef}
+            style={{
+                maxWidth: videoMaxWidth,
+                maxHeight: videoMaxHeight,
+            }}
+        />
+        <button className={styles.button}>Take Screenshot</button>
+    </div>);
 
-export default class VideoPlayer extends Component {
-    state = {
-        videoLoading: true
-    };
+VideoPlayer.propTypes = {
+    style: PropTypes.object,
+    videoMaxWidth: PropTypes.number,
+    videoMaxHeight: PropTypes.number,
+    videoRef: PropTypes.func,
+};
 
-    componentDidUpdate(prevProps, prevState) {        
-        // check if stream has updated
-        // if yes then play the video
-        if (this.props.stream !== prevProps.stream) {
-            const { error, loading, stream } = this.props;
-            if (!error && !loading) {
-                this.video.srcObject = stream;
-                this.setState({ videoLoading: true });
-                this.video.play().then(() => {
-                    this.setState({ videoLoading: false });
-                    this.props.onVideoPlayingStart();
-                });
-            }
-        }
-    }
+VideoPlayer.defaultProps = {
+    style: {},
+    videoMaxWidth: null,
+    videoMaxHeight: null,
+    videoRef: null,
+};
 
-    render() {
-        const { error, loading } = this.props;
-        return <div className={classNames(styles.container, styles.center)}>
-            {(loading || this.state.videoLoading)?
-                (<div className={styles.center}>
-                    <ScaleLoader 
-                        height={30}
-                        margin='2px'
-                        radius={2}
-                        width={4}
-                    />
-                </div>) : null
-            }
-            {error ?
-                (<div className={styles.center}>
-                    <ErrorIcon width={160} style={{ color: '#b60000' }}/>
-                    <div className={styles.errorText}>Unable to load video</div>
-                </div>) : null
-            }
-            {(!error && !loading) ?
-                (<div className={styles.center}>
-                    <video 
-                        className={classNames(styles.video)}
-                        muted
-                        ref={ref => this.video = ref}
-                        style={{
-                            visibility: this.state.videoLoading ? 'hidden' : 'visible'
-                        }}
-                    />
-                    <button>Take Screenshot</button>
-                </div>) : null
-            }
-        </div>
-    }
-}
+export default VideoPlayer;
