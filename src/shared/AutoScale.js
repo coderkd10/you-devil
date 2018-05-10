25fb8ed22b511a93c857ee910dd279b40adde9a2
Abhishek Kedia <kedia.abhishek10@gmail.com>
2018-05-10 19:47:20 +0530

    Generalize autoscaling to both x and y dimensions

--- src/shared/AutoScale.js
+++ src/shared/AutoScale.js
@@ -1,10 +1,11 @@
 // Takes a input component and applies scaling transform on its
-// so that it can fit inside a *fixed width* parent component
+// so that it can fit inside a fixed height / width parent component
 // TODO : Publish this as a standalone library in npm
 
-// This component takes target container width as prop
+// This component takes target container width, height as prop
 // and a render prop, and appropriately scales the render prop
-// component so that it fits with the input container width, i.e,
+// component so that it fits with the input container width, height, i.e,
+// rendered component's height <= container height and
 // rendered component's width <= container width
 
 import React, { Component } from 'react';
@@ -37,60 +38,85 @@ import Measure from 'react-measure';
 // (But in this case its not tracking the changes caused by scaling changing has made the problem somewhat
 // simpler for us).
 
-const getScale = (baseWidth, containerWidth) => {
+const getScale = (baseDimension, containerDimension) => {
     // computes what is the desired scale that we should use
-    // so that element with baseWidth fits inside containerWidth
-    if (containerWidth >= baseWidth) {
+    // so that element with base dimension fits inside container dimension
+    if (containerDimension >= baseDimension) {
         return 1;
     }
-    return containerWidth / baseWidth;
+    return containerDimension / baseDimension;
+}
+const getOverallScale = (baseWidth, baseHeight, containerWidth, containerHeight) => {
+    const xScale = getScale(baseWidth, containerWidth);
+    const yScale = getScale(baseHeight, containerHeight);
+    return Math.min(xScale, yScale);
 }
 
-class HorizontalAutoScale extends Component {
+class AutoScale extends Component {
     state = {
-        // unscaled width of the input component
+        // unscaled dimensions of the input component
         baseWidth: null,
         baseHeight: null,
         // current scale factor used in scaling transform
         scale: 1,
         // used to track change in container dimensions
-        lastContainerWidth: null
+        lastContainerWidth: null,
+        lastContainerHeight: null,
     };
 
     onResize = ({ bounds }) => {
-        // This is fired only basewidth changes
+        // This is fired only base dimensions change
         // but we've written in a way that if this behavior changes
         // in the future we'll be unaffected.
         
         // Basically the job of this method is only the update the base
-        // width (along with new scale) whenever it changes and 
+        // width, height (along with new scale) whenever it changes and
         // ignore everything else
         const { width, height } = bounds;
+        
         const newBaseWidth = width / this.state.scale;
-        if (newBaseWidth !== this.state.baseWidth) {
-            // base width has updated need to update it along with scale
+        const newBaseHeight = height / this.state.scale;
+
+        if ((newBaseWidth !== this.state.baseWidth) || 
+            (newBaseHeight !== this.state.baseHeight)) {
+            // base width / height has updated need to update it along with scale
+            const newScale = getOverallScale(
+                newBaseWidth,
+                newBaseHeight,
+                this.props.containerWidth,
+                this.props.containerHeight);
             this.setState({
                 baseWidth: newBaseWidth,
-                baseHeight: height / this.state.scale,
-                scale: getScale(newBaseWidth, this.props.containerWidth)
+                baseHeight: newBaseHeight,
+                scale: newScale,
             });
         }
     }
 
     static getDerivedStateFromProps(nextProps, prevState) {
-        if ((nextProps.containerWidth === prevState.lastContainerWidth) || 
-            (!prevState.baseWidth)) {
-            // either containerWidth did not update
-            // or we don't know baseWidth yet. 
+        if ((nextProps.containerWidth === prevState.lastContainerWidth && 
+            nextProps.containerHeight === prevState.lastContainerHeight) ||
+            !prevState.baseWidth || !prevState.baseHeight) {
+            // either container dimensions did not update
+            // or we don't know base dimensions yet.
             // no point in updating the state here
             return null;
         }
         
         const newContainerWidth = nextProps.containerWidth;
+        const newContainerHeight = nextProps.containerHeight;
+        const newScale = getOverallScale(
+            prevState.baseWidth,
+            prevState.baseHeight,
+            newContainerWidth,
+            newContainerHeight
+        );
+
         return {
             ...prevState,
-            scale: getScale(prevState.baseWidth, newContainerWidth),
+            scale: newScale,
             lastContainerWidth: newContainerWidth,
+            lastContainerHeight: newContainerHeight,
         };
     }
 
@@ -110,12 +136,18 @@ class HorizontalAutoScale extends Component {
     }
 
     render() {
+        const style = {
+            overflow: 'hidden',
+            whiteSpace: 'noWrap',
+        };
+        if (isFinite(this.props.containerWidth)) {
+            style.maxWidth = this.props.containerWidth;
+        }
+        if (isFinite(this.props.containerHeight)) {
+            style.maxHeight = this.props.containerHeight;
+        }
         return (
-            <div style={{
-                overflow: 'hidden',
-                whiteSpace: 'nowrap',
-                maxWidth: this.props.containerWidth,
-            }}>
+            <div style={{ ...style, ...this.props.style }}>
                 <Measure bounds onResize={this.onResize}>
                     {({ measureRef }) =>
                         <div ref={measureRef} style={{
@@ -131,14 +163,21 @@ class HorizontalAutoScale extends Component {
     }
 }
 
-HorizontalAutoScale.propTypes = {
-    containerWidth: PropTypes.number.isRequired,
+AutoScale.propTypes = {
+    containerWidth: PropTypes.number, // can be infinity if don't want to constrain width
+    containerHeight: PropTypes.number, // can be infinity if don't want to constrain height
+
     render: PropTypes.func.isRequired,
     onResize: PropTypes.func, // this callback gets called whenever inner's size gets updated
+
+    style: PropTypes.object,
 };
 
-HorizontalAutoScale.defaultProps = {
-    onResize: console.log
+AutoScale.defaultProps = {
+    containerWidth: Infinity,
+    containerHeight: Infinity,
+    onResize: () => {},
+    style: {},
 };
 
-export default HorizontalAutoScale;
+export default AutoScale;
